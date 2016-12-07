06e9b781d77c61a51107110fed828e82793ede70
Ahmed Ghannam <aghannam@outlook.com>
2016-12-06 21:02:23 -0600

    Update Pair.java

--- org/aghannam/bpcp/Pair.java
+++ org/aghannam/bpcp/Pair.java
@@ -5,26 +5,30 @@
 package org.aghannam.bpcp;
 
 /**
- * This class represents a pair of a BPCP instance. 
+ * This class represents a corresponding pair of a BPCP instance.
  * <p>
- * Intuitively, a pair resembles a numerical fraction, with 
- * one string at the top and another at the bottom. A pair is also known as a domino or 
- * a tile in some textbooks. A typical input instance is a list of one or more pairs. 
- *  
+ * Intuitively, a corresponding pair resembles a numerical fraction, with one
+ * string at the top and another at the bottom. A pair is also known as a domino
+ * or a tile in some textbooks. A typical input instance is a list of one or
+ * more such pairs.
+ * 
  * @author Ahmed Ghannam (amalghannam@crimson.ua.edu)
  */
 public class Pair {
 	protected String top;
 	protected String bottom;
-	
+
 	/**
-	 * Constructs a new pair of a BPCP instance, given two strings.
+	 * Constructs a new corresponding pair of a BPCP instance, given two
+	 * strings.
 	 * 
-	 * @param top the top string of the pair
-	 * @param bottom the bottom string of the pair
+	 * @param top
+	 *            the top string of the pair
+	 * @param bottom
+	 *            the bottom string of the pair
 	 */
 	public Pair(String top, String bottom) {
-		this.top = top; 
-		this.bottom = bottom; 
+		this.top = top;
+		this.bottom = bottom;
 	}
 }
